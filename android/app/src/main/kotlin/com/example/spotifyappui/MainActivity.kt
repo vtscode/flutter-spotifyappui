e65ecca420f71a7e54ea490c362aab88d8ae081c
vtscode <ventuscode@gmail.com>
2023-04-11 16:36:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/spotifyappui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spotifyappui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import './data/data.dart';
+import './models/current_track_model.dart';
+import './screens/playlist_screen.dart';
+import './widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 800));
+  }
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CurrentTrackModel(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Spotify UI',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        primaryColor: Colors.black,
+        iconTheme: const IconThemeData().copyWith(color: Colors.white),
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          displayMedium: const TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 12.0,
+            color: Colors.grey[300],
+            fontWeight: FontWeight.w500,
+            letterSpacing: 2.0,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.grey[300],
+            fontSize: 14.0,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.0,
+          ),
+          bodyMedium: TextStyle(
+            color: Colors.grey[300],
+            letterSpacing: 1.0,
+          ),
+        ),
+        colorScheme: ColorScheme.light().copyWith(
+          brightness: Brightness.dark,
+          secondary: const Color(0xFF1DB954),
+        ),
+      ),
+      home: Shell(),
+    );
+  }
+}
+
+class Shell extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                if (MediaQuery.of(context).size.width > 800) SideMenu(),
+                const Expanded(
+                  child: PlaylistScreen(playlist: lofihiphopPlaylist),
+                ),
+              ],
+            ),
+          ),
+          CurrentTrack(),
+        ],
+      ),
+    );
+  }
+}
